7754ac4143425947a41cf0ce973bb633c5cdf110
Segev <Segev.k@ez-roi.com>
2020-11-25 21:24:25 +0200

    Tester2 - Point Class Done

--- Maman12/src/PointTester2.java
+++ Maman12/src/PointTester2.java
@@ -11,42 +11,56 @@ public class PointTester2
     public static void main(String[] args)
     {
         System.out.println("============ Testing class Point =============");
-        Point p1 = new Point(3.0, 4.0);
+        Point p1 = new Point(-100.0, 0.1);
         	System.out.println("After constructor P1 "+p1);
         Point p2 = new Point(p1);
+        
+        p1.move(2.0, 2.0);
         	System.out.println("After constructor P2 :"+ p2);
         double d1 = p1.getX();
         	System.out.println("X of p1:" +d1);	
         d1 = p1.getY();
     		System.out.println("Y of p1:"+d1);	
 
-        if (!p1.equals(p2))
-            System.out.println("Check your equals method in class Point");
-        //p1.setX(0.0);
-    		//System.out.println("After SetX:"+ p1);	
-
-        p1.setY(0.0);
-		System.out.println("After SetY:"+ p1);	
-
+    	
+        //if (!p1.equals(p2))
+            //System.out.println("Check your equals method in class Point");
+        p2.setY(5.0);
+    		//System.out.println("After SetX:"+ p1);	        
         boolean b1 = p1.isAbove(p2); 
-		System.out.println("isAbove:"+ b1 +". Abs: "+Math.abs(p1.getY()-p2.getY()));	
+		System.out.println("isAbove:"+ b1 +". Abs: "+Math.abs(p1.getY()-p2.getY()));
+		
+		p1.setX(-3.0);
+		p2.setX(3.0);
+		
 
-        b1 = p1.isUnder(p2);
-        System.out.println("isUnder:"+ b1);	
-        b1 = p1.isLeft(p2);
-        System.out.println("isLeft:"+ b1 +". Abs: "+Math.abs(p1.getX()-p2.getX()));	
+        
+    	System.out.println(" P1 :"+ p1);
+    	System.out.println(" P2 :"+ p2);
+    	
+    	
         b1 = p1.isRight(p2);
         System.out.println("isRight:"+ b1 +" p1X :" +p1.getX()+", p2X: "+p2.getX());	
+        p1.setX(0.0);
+		System.out.println("After SetY:"+ p1);	
+
+		
+        b1 = p2.isRight(p1);
+        System.out.println("isRight:"+ b1 +" p1X :" +p1.getX()+", p2X: "+p2.getX());	
         
-        d1 = p1.distance(p2);
+        d1 = p2.distance(p1);
         System.out.println("distance:"+ d1);	
+        
+        p2.move(-2.0, 2.0);
 
-        p1.move(1.0, 2.0);
-    	System.out.println("After MOVE "+p1);
-
-        if (p1.toString().indexOf('@') != -1)
-            System.out.println("Check your toString method in class Point");
-        System.out.println("============ Testing class Point Done =============");        
+        
+    	System.out.println(" P1 :"+ p1);
+    	System.out.println(" P2 :"+ p2);
+    	
+       
+        
+        
+        
     }
 }
 
